72717bc3ef124e47ee07f23589eb63d07945887e
lavanyagarg112 <lavanyagarg@u.nus.edu>
2025-05-20 13:36:32 +0800

    Add connection to db check

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
   res.send("Welcome to PostgreSQL with Node.js and Express!");
 });
 
+app.get("/checkconnection", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM check_connection");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error connecting:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
