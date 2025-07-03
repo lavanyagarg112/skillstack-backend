7d1c38c24f73d96bc1e3aa8762c1473600dca97e
lavanyagarg112 <lavanyagarg@u.nus.edu>
2025-07-03 08:13:50 +0200

    initialise routers routes file

---
+++ routes/reports.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const pool = require("../database/db");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+
+module.exports = router;
